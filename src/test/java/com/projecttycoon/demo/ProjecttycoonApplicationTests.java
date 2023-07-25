3c35719f038bda4e56fd11f0e83b58adec7a062f
ReverserofCode <patteon@naver.com>
2023-07-25 10:53:55 +0900

    프로젝트 기초 구조 작성

---
+++ src/test/java/com/projecttycoon/demo/ProjecttycoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecttycoon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttycoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
