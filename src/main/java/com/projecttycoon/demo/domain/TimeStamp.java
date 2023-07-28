431ad3b4e60be6f35972b7591b9cea2a6074711f
ReverserofCode <patteon@naver.com>
2023-07-28 18:53:34 +0900

    서버 TimeZone 재설정 실 서버에서 구동시 제대로 된 시간이 기입되지 않음

--- src/main/java/com/projecttycoon/demo/domain/TimeStamp.java
+++ src/main/java/com/projecttycoon/demo/domain/TimeStamp.java
@@ -3,7 +3,6 @@ package com.projecttycoon.demo.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
