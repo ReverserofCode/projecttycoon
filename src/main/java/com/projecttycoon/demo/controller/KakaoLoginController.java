29be7fcd96caa096c6ad8ea20b2257d7b3974c7c
ReverserofCode <patteon@naver.com>
2023-08-07 16:27:53 +0900

    08 07 카카오 로그인 서비스 책임 분리 및 토큰 요청 테스트

--- src/main/java/com/projecttycoon/demo/controller/KakaoLoginController.java
+++ src/main/java/com/projecttycoon/demo/controller/KakaoLoginController.java
@@ -1,5 +1,6 @@
 package com.projecttycoon.demo.controller;
 
+import com.projecttycoon.demo.domain.service.KakaoLoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.*;
@@ -21,32 +22,7 @@ public class KakaoLoginController {
     @ResponseBody
     @GetMapping("/kakao")
     public ResponseEntity<String> kakaoCallBack(@RequestParam String code) throws Exception {
-
-        log.info("Call Post logic");
-
-        final String grant_type = "authorization_code";
-        final String client_id = "777cadaafac1cb2cb8aa5dc765cde3f4";
-        final String redirect_uri = "http://localhost:9999/auth/kakao";
-        final String api_Url = "https://kauth.kakao.com/oauth/token";
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", grant_type);
-        params.add("client_id", client_id);
-        params.add("redirect_uri", redirect_uri);
-        params.add("code",code);
-
-        ResponseEntity<String> responseEntity = new RestTemplate()
-                .postForEntity(api_Url, params, String.class);
-
-        if(responseEntity.getStatusCode().is2xxSuccessful()){
-            log.info("Success response");
-            String responseBody = responseEntity.getBody();
-            log.info(responseBody);
-            return responseEntity;
-        }
-        else {
-            log.info("Fail login response");
-            return null;
-        }
+        KakaoLoginService kakaoLoginService = new KakaoLoginService();
+        return kakaoLoginService.kakaoLoginProcess(code);
     }
 }
--- src/main/java/com/projecttycoon/demo/domain/service/KakaoLoginService.java
+++ src/main/java/com/projecttycoon/demo/domain/service/KakaoLoginService.java
@@ -1,6 +1,41 @@
 package com.projecttycoon.demo.domain.service;
 
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
 public class KakaoLoginService {
 
+    public ResponseEntity<String> kakaoLoginProcess(String code) throws Exception{
+
+        log.info("Call Post logic");
+
+        final String grant_type = "authorization_code";
+        final String client_id = "777cadaafac1cb2cb8aa5dc765cde3f4";
+        final String redirect_uri = "http://localhost:9999/auth/kakao";
+        final String api_Url = "https://kauth.kakao.com/oauth/token";
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", grant_type);
+        params.add("client_id", client_id);
+        params.add("redirect_uri", redirect_uri);
+        params.add("code", code);
+
+        ResponseEntity<String> responseEntity = new RestTemplate().postForEntity(api_Url, params, String.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            log.info("Success response");
+            String responseBody = responseEntity.getBody();
+            log.info(responseBody);
+            return responseEntity;
+        } else {
+            log.info("Fail login response");
+            return null;
+        }
+
+    }
 }
