ddd9783ad9af596daf708073ee13db273c04160f
ReverserofCode <patteon@naver.com>
2023-08-08 18:11:35 +0900

    08 08 카카오 로그인 정보 출력

--- src/main/java/com/projecttycoon/demo/controller/KakaoLoginController.java
+++ src/main/java/com/projecttycoon/demo/controller/KakaoLoginController.java
@@ -1,13 +1,25 @@
 package com.projecttycoon.demo.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.projecttycoon.demo.domain.service.KakaoLoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Log4j2
 @Component
@@ -17,6 +29,7 @@ public class KakaoLoginController {
 
     @Autowired
     KakaoLoginService kakaoLoginService;
+
     @ResponseBody
     @GetMapping("/kakao")
     public ResponseEntity<String> kakaoCallBack(@RequestParam String code) throws Exception {
--- src/main/java/com/projecttycoon/demo/domain/service/KakaoLoginService.java
+++ src/main/java/com/projecttycoon/demo/domain/service/KakaoLoginService.java
@@ -1,16 +1,24 @@
 package com.projecttycoon.demo.domain.service;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.core.config.plugins.convert.TypeConverters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Log4j2
 @Service
@@ -22,14 +30,17 @@ public class KakaoLoginService {
     String redirectUri;
     @Value("${kakao-ApiUrl}")
     String kakaoApiUrl;
+    @Value("${kakao-dataUrl}")
+    String dataUrl;
 
-    public ResponseEntity<String> kakaoLoginProcess(String code) throws Exception{
+
+    public ResponseEntity<String> kakaoLoginProcess(String code) throws Exception {
 
         log.info("Call Post logic");
         final String grant_type = "authorization_code";
-        log.info("Call KakaoRestApiKey : "+kakaoRestAPIKey);
-        log.info("Call redirect uri : "+redirectUri);
-        log.info("Call KakaoRestAPiKEY : "+kakaoApiUrl);
+        log.info("Call KakaoRestApiKey : " + kakaoRestAPIKey);
+        log.info("Call redirect uri : " + redirectUri);
+        log.info("Call KakaoRestAPiKEY : " + kakaoApiUrl);
 
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
@@ -44,11 +55,41 @@ public class KakaoLoginService {
             log.info("Success response");
             String responseBody = responseEntity.getBody();
             log.info(responseBody);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+            String accessToken = jsonNode.get("access_token").asText();
+            String tokenType = jsonNode.get("token_type").asText();
+
+            log.info("Access Token : " + accessToken);
+            log.info("Token Type : " + tokenType);
+            log.info("dataReturn : " + getAgreementInfo(accessToken, tokenType));
+
             return responseEntity;
         } else {
             log.info("Fail login response");
             return null;
         }
+    }
 
+    public ResponseEntity<String> getAgreementInfo(String accessToken, String tokenType) throws Exception {
+
+        log.info("Call getAgreement");
+        log.info("call AccessToken : " + accessToken);
+        log.info("Call DataUrl : "+dataUrl);
+
+        // RestTemplate 생성
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Authorization", "Bearer "+accessToken);
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String,String>> kakaoProfileRequest = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(dataUrl, HttpMethod.GET, kakaoProfileRequest, String.class);
+        log.info(response.getBody());
+
+        return response;
     }
 }
