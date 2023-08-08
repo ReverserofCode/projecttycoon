729dca8ca5190619612b6a66e6234ede5feaf9bd
ReverserofCode <patteon@naver.com>
2023-08-08 12:42:01 +0900

    08 08 카카오 로그인 임시 종결

--- src/main/java/com/projecttycoon/demo/controller/KakaoLoginController.java
+++ src/main/java/com/projecttycoon/demo/controller/KakaoLoginController.java
@@ -5,26 +5,39 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @Log4j2
 @Component
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/auth")
 public class KakaoLoginController {
 
     @ResponseBody
     @GetMapping("/kakao")
     public ResponseEntity<String> kakaoCallBack(@RequestParam String code) throws Exception {
+        log.info("call /auth/kakao");
         KakaoLoginService kakaoLoginService = new KakaoLoginService();
         return kakaoLoginService.kakaoLoginProcess(code);
     }
 
-    @GetMapping("/loginProcessCall")
-    public void kakaoLoginProcessCall(){
-        
-    }
+//    @GetMapping("/kakaoProcessCall")
+//    public String kakaoLoginProcessCall() {
+//
+//        log.info("call kakaoProcessCall");
+//        final String kakaoLoginURL = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=777cadaafac1cb2cb8aa5dc765cde3f4&redirect_uri=http://projecttycoon.com/auth/kakao";
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<String> responseEntity = restTemplate.getForEntity(kakaoLoginURL, String.class);
+//        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+//            log.info("KakaoLogin Process Accept");
+//            String responseBody = responseEntity.getBody();
+//            log.info("Response : " + responseBody);
+//            return responseBody;
+//        } else {
+//            log.info("Failed to get data. Status code : " + responseEntity.getStatusCode());
+//            return null;
+//        }
+//    }
 }
--- src/main/java/com/projecttycoon/demo/domain/service/KakaoLoginService.java
+++ src/main/java/com/projecttycoon/demo/domain/service/KakaoLoginService.java
@@ -3,11 +3,13 @@ package com.projecttycoon.demo.domain.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Log4j2
+@Service
 public class KakaoLoginService {
 
     public ResponseEntity<String> kakaoLoginProcess(String code) throws Exception{
