6807d1bbc0ec42ea58e2b60c657ef25ca558522a
hyerimlee <hrim28@icloud.com>
2023-08-21 17:11:51 +0900

    백엔드마스터->불렛인보드(백업) 소스복사

---
+++ src/main/java/com/projecttycoon/demo/controller/UploadController.java
@@ -0,0 +1,29 @@
+package com.projecttycoon.demo.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@RestController
+@Log4j2
+public class UploadController {
+
+
+    @PostMapping("/uploadTest")
+    public void uploadTest(@RequestParam MultipartFile file) throws Exception {
+        final String uploadDir = "/home/UploadTest";
+
+        if (!file.isEmpty()) {
+            String filename = file.getOriginalFilename();
+            log.info("Origin File Name : " + filename);
+            String fullPath = uploadDir + File.separator + filename;
+            log.info(fullPath);
+//           실제로 파일 저장을 실행하는 부분
+            file.transferTo(new File(fullPath));
+        }
+    }
+}
