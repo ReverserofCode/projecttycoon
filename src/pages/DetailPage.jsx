550bd266e59424e3e8d3188219091c85c78a5a4f
minicastle <jk598200167@gmail.com>
2023-08-28 20:25:57 +0900

    [rework] 디테일 페이지

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import styled from "@emotion/styled";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import Sidebar from "../components/Sidebar";
+import purify from "dompurify";
 import { GetProjectFromID } from "../functional/GetProject";
 
 const Container = styled.div`
@@ -31,8 +32,76 @@ const MainContents = styled.div`
   align-items: center;
   margin-left: 50px;
   width: 100%;
+  max-width: 1000px;
 `;
-
+const MainTitle = styled.h2`
+  width: 100%;
+  border-bottom: 5px solid #d9d9d9;
+  padding: 10px 0;
+`;
+const Poster = styled.img`
+  width: 100%;
+  border-top: 5px solid #35a29f;
+  border-bottom: 5px solid #35a29f;
+`;
+const ProjectInfos = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 20px 15px;
+  border-top: 5px solid #d9d9d9;
+  border-bottom: 5px solid #d9d9d9;
+  margin-top: 20px;
+`;
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 40%;
+  gap: 15px;
+`;
+const InfoRow = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  text-align: center;
+  width: 100%;
+  font-size: 20px;
+  gap: 15px;
+`;
+const InfoCell = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+const InfoTitle = styled.span``;
+const InfoValue = styled.span`
+  font-weight: 600;
+`;
+const InfoDate = styled.span`
+  color: #949494;
+  white-space: nowrap;
+`;
+const Preview = styled.div`
+  margin-top: 20px;
+  display: flex;
+  width: 100%;
+  box-sizing: border-box;
+  flex-direction: column;
+  border: 2px solid #c4c4c4;
+  box-shadow: rgb(0 0 0 / 20%) 0px 1rem 2rem;
+  border-radius: 5px;
+  padding: 10px;
+  .ql-syntax {
+    border: 2px solid #c4c4c4;
+    padding: 10px;
+    background-color: #ffffebd1;
+  }
+`;
 function DetailPage() {
   const value = {
     createdAt: "2023-08-19T06:54:37.741+00:00",
@@ -41,6 +110,8 @@ function DetailPage() {
     projectTitle: "Test title item length test. how long text viewing",
     projectContent: "testContents",
     projectWantedRole: [
+      { Role: "back", complete: 1, personnel: 3 },
+      { Role: "front", complete: 0, personnel: 2 },
       { Role: "back", complete: 1, personnel: 3 },
       { Role: "front", complete: 0, personnel: 2 },
     ],
@@ -54,6 +125,18 @@ function DetailPage() {
     projectFileName: "projectImage",
     projectScrapNum: 10,
   };
+  const handleRecruits = useCallback(() => {
+    let contents = [];
+    for (let i = 0; i < value?.projectWantedRole.length; i++) {
+      contents.push(
+        <InfoRow>
+          <InfoValue>{value?.projectWantedRole[i].Role}</InfoValue>
+          <InfoValue>{value?.projectWantedRole[i].personnel}명</InfoValue>
+        </InfoRow>
+      );
+    }
+    return contents;
+  }, [value?.projectWantedRole]);
   useEffect(() => {
     const path = window.location.href.split("/");
     // console.log(path[5]);
@@ -65,7 +148,34 @@ function DetailPage() {
         <AiOutlineArrowLeft fontSize={"30px"} />
         <Sidebar value={value} />
       </SideContents>
-      <MainContents></MainContents>
+      <MainContents>
+        <MainTitle>{value?.projectTitle}</MainTitle>
+        <Poster src={"http://projecttycoon.com" + value?.projectFilePath} />
+        <ProjectInfos>
+          <InfoColumn>
+            <InfoRow>
+              <InfoTitle>{value?.projectWriterNick}</InfoTitle>
+              <InfoDate>{value?.createdAt.slice(0, 10)}</InfoDate>
+            </InfoRow>
+            <InfoRow>
+              <InfoTitle>모집인원</InfoTitle>
+              <InfoCell>{handleRecruits()}</InfoCell>
+            </InfoRow>
+          </InfoColumn>
+          <InfoColumn>
+            <InfoRow></InfoRow>
+            <InfoRow>
+              <InfoTitle>시작예정일</InfoTitle>
+              <InfoValue>{value?.projectDue.slice(0, 10)}</InfoValue>
+            </InfoRow>
+            <InfoRow>
+              <InfoTitle>진행학원</InfoTitle>
+              <InfoValue>{value?.projectAcademy} 아카데미</InfoValue>
+            </InfoRow>
+          </InfoColumn>
+        </ProjectInfos>
+        <Preview dangerouslySetInnerHTML={{ __html: purify.sanitize() }} />
+      </MainContents>
     </Container>
   );
 }
