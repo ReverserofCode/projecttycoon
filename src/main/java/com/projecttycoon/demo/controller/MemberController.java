c325bb9afe7b5a222b3de2b2729ef83cf2eaf7fa
jok6965 <jok6965@naver.com>
2023-09-03 00:19:37 +0900

    0903 mypage,memberpage 데이터 가져오기

--- src/main/java/com/projecttycoon/demo/controller/MemberController.java
+++ src/main/java/com/projecttycoon/demo/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.projecttycoon.demo.controller;
 
+import com.projecttycoon.demo.domain.Entity.MemberEntity;
 import com.projecttycoon.demo.domain.dto.MemberLoginDTO;
 import com.projecttycoon.demo.domain.dto.MemberRequestDTO;
+import com.projecttycoon.demo.domain.repository.MemberRepository;
 import com.projecttycoon.demo.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MemberController {
 
+    final MemberRepository memberRepository;
     MemberService memberService;
 
     @Autowired
-    MemberController(MemberService memberService) {
+    MemberController(MemberService memberService, MemberRepository memberRepository) {
         this.memberService = memberService;
+        this.memberRepository = memberRepository;
     }
 
     @GetMapping("/api/callTest")
@@ -56,6 +60,18 @@ public class MemberController {
     @DeleteMapping("/api/memberDelete/{memberId}")
     public void deleteDb(@PathVariable String memberId) {
         memberService.memberDelete(memberId);
+    }
+
+    @GetMapping("/api/memberPage/{memberId}")
+    public MemberRequestDTO memberPage(@PathVariable String memberId) {
+        MemberRequestDTO memberRequestDTO = new MemberRequestDTO(memberRepository.findByMemberId(memberId).orElseThrow());
+        return memberRequestDTO;
+    }
+
+    @GetMapping("/api/mypage")
+    public MemberRequestDTO mypage(@AuthenticationPrincipal MemberLoginDTO memberLoginDTO) {
+        MemberRequestDTO memberRequestDTO = new MemberRequestDTO(memberRepository.findByMemberId(memberLoginDTO.getMemberId()).orElseThrow());
+        return memberRequestDTO;
     }
 
 }
