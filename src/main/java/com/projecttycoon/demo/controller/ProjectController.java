d77d54e5d9125993f31ee649a1d6a5a0fa7c7f69
ReverserofCode <patteon@naver.com>
2023-09-05 11:27:31 +0900

    09 03 프로젝트 상세 페이지 호출 구현

--- src/main/java/com/projecttycoon/demo/controller/ProjectController.java
+++ src/main/java/com/projecttycoon/demo/controller/ProjectController.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
@@ -55,7 +56,7 @@ public class ProjectController {
 
 
     @PostMapping("/projectRegister")
-    public String saveProject(@RequestPart(value = "file") MultipartFile file, @RequestPart(value = "projectRequestDTO") ProjectRequestDTO projectRequestDTO) throws Exception {
+    public String saveProject(@RequestPart(value = "file", required = false) MultipartFile file, @RequestPart(value = "projectRequestDTO") ProjectRequestDTO projectRequestDTO) throws Exception {
         log.info("Call Method saveProject ");
         projectService.createProject(projectRequestDTO, file);
         return "success";
@@ -84,7 +85,7 @@ public class ProjectController {
 
     //1개의프로젝트가져오기
     @GetMapping("/project/{id}")
-    public ProjectEntity listone(@PathVariable Long id) {
+    public ProjectEntity listOne(@PathVariable Long id) {
 //        ProjectEntity pe = new ProjectEntity();
 //        pe = projectRepository.findById(id).get();
 //        return pe;
--- src/main/java/com/projecttycoon/demo/controller/WebController.java
+++ src/main/java/com/projecttycoon/demo/controller/WebController.java
@@ -1,12 +1,18 @@
 package com.projecttycoon.demo.controller;
 
+import com.projecttycoon.demo.domain.Entity.ProjectEntity;
+import com.projecttycoon.demo.domain.repository.ProjectRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.swing.text.html.Option;
 import javax.websocket.server.PathParam;
+import java.util.Optional;
 
 
 //웹 사이트 내에서 다른 사이트에 대한 요청을 처리하는 컨르롤러
@@ -14,13 +20,16 @@ import javax.websocket.server.PathParam;
 @Controller
 public class WebController {
 
+   ProjectRepository projectRepository;
+   @Autowired
+   public WebController(ProjectRepository projectRepository){
+       this.projectRepository = projectRepository;
+   }
+
+
     @RequestMapping("/api/login")
     public String initiatingLogin() {
         return "/static/PageLogin/index.html";
-    }
-
-    @RequestMapping("/uploadEx")
-    public void fileUpload() {
     }
 
     @GetMapping("/")
@@ -49,8 +58,25 @@ public class WebController {
     }
 
     @GetMapping("/callPageProjectBoardDetail")
-    public String callPageProjectBoardDetail() {
-        return "/static/PageProjectBoardDetail/index.html";
+    public String callPageBoardDetail(){
+       return "/static/PageProjectBoardDetail/index.html";
     }
+
+    @GetMapping("/callPageProjectBoardDetail/{id}")
+    public ModelAndView callPageProjectBoardDetail(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView();
+
+        Optional<ProjectEntity> object = projectRepository.findById(id);
+
+        if (object.isPresent()) {
+
+            log.info("call Detail Page and target Object detected");
+            mav.addObject("projectDTO", object.get());
+            mav.setViewName("forward:/static/PageProjectBoardDetail/index.html");
+        }
+
+        return mav;
+    }
+
 
 }
