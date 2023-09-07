59802d67e02dff69a8ea6fde4c6c545438c0b2d3
minicastle <jk598200167@gmail.com>
2023-09-07 14:44:03 +0900

    [feat] login detect, logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import styled from "@emotion/styled";
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
+import { LoginCheck } from "../functional/LoginCheck";
+import { Logout } from "../functional/Logout";
 const Container = styled.div`
   display: flex;
   box-sizing: border-box;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   padding: 0 30px;
   width: 100%;
   border-bottom: 1px solid #071952;
-  @media screen and (min-width: 700px) {
+  @media screen and (max-width: 700px) {
     height: 64px;
   }
 `;
@@ -30,6 +31,9 @@ const Lists = styled.ul`
   color: ${(props) => {
     return props.color ? props.color : "#071952";
   }};
+`;
+const UserIcon = styled.image`
+  height: 50px;
 `;
 const Items = styled.li`
   font-size: 20px;
@@ -52,36 +56,57 @@ const Wrap = styled.div`
 `;
 
 function Navbar() {
-  const { params } = useParams();
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    LoginCheck().then((res) => {
+      setUserData(res);
+    });
+  }, []);
   return (
     <Container>
       <Wrap>
-        <NavHeader src="./Logo Test.png" />
+        <NavHeader src="http://projecttycoon.com/static/images/Logo Test.png" />
         <Lists gap={"30px"}>
           <Link className="menu" href="http://projecttycoon.com">
             <Items>홈</Items>
           </Link>
-          <Link className="menu" href="http://projecttycoon.com">
+          <Link className="menu" href="http://projecttycoon.com/projects">
             <Items>프로젝트 팀</Items>
           </Link>
-          <Link className="menu" href="http://projecttycoon.com">
+          <Link className="menu" href="http://projecttycoon.com/members">
             <Items>멤버리스트</Items>
           </Link>
-          <Link className="menu" href="http://projecttycoon.com">
+          <Link className="menu" href="http://projecttycoon.com/myPage">
             <Items>내 페이지</Items>
           </Link>
-          <Link className="menu" href="http://projecttycoon.com">
+          <Link
+            className="menu"
+            href="http://www.projecttycoon.com/callPageNewProject"
+          >
             <Items>새로운 글쓰기</Items>
           </Link>
         </Lists>
-        <Lists gap={"47px"} color="#35A29F">
-          <Link className="user" href="http://projecttycoon.com/api/login">
-            <Items>로그인</Items>
-          </Link>
-          <Link className="user" href="http://projecttycoon.com/api/signup">
-            <Items>회원가입</Items>
-          </Link>
-        </Lists>
+        {userData === "" ? (
+          <Lists gap={"47px"} color="#35A29F">
+            <Link className="user" href="http://projecttycoon.com/api/login">
+              <Items>로그인</Items>
+            </Link>
+            <Link className="user" href="http://projecttycoon.com/api/signup">
+              <Items>회원가입</Items>
+            </Link>
+          </Lists>
+        ) : (
+          <Lists
+            gap={"20px"}
+            color="#35A29F"
+            onClick={() => {
+              Logout();
+            }}
+          >
+            <UserIcon src={userData?.memberFilePath} />
+            <Items>로그아웃</Items>
+          </Lists>
+        )}
       </Wrap>
     </Container>
   );
