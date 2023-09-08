90fa54c3cded18c4a6e83f658e016d659ca5ee79
minicastle <jk598200167@gmail.com>
2023-09-08 15:56:48 +0900

    [fix] role

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -65,7 +65,7 @@ function ProjectPage() {
       );
       let bufRoleValue = [];
       for (let j = 0; j < bufRole.length; j++) {
-        bufRoleValue.push(bufRole[j].role);
+        bufRoleValue.push(bufRole[j].Role);
       }
       contents.push(
         <BoardItem
