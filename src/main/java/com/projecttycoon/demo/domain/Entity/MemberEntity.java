f6194d3b863f23fd0b7874f5034012ab04614972
hyerimlee <hrim28@icloud.com>
2023-10-10 13:54:33 +0900

    멤버 클래스 머지

--- src/main/java/com/projecttycoon/demo/domain/Entity/MemberEntity.java
+++ src/main/java/com/projecttycoon/demo/domain/Entity/MemberEntity.java
@@ -1,38 +1,55 @@
 package com.projecttycoon.demo.domain.Entity;
 
 import com.projecttycoon.demo.domain.TimeStamp;
+import com.projecttycoon.demo.domain.dto.MemberLoginDTO;
 import com.projecttycoon.demo.domain.dto.MemberRequestDTO;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "MemberData")
 @EntityListeners(AuditingEntityListener.class)
 public class MemberEntity extends TimeStamp {
 
     @Id
-    @Column(name = "memberid", unique = true)
+    @Column(name = "memberId", unique = true)
     private String memberId;
-    @Column(name = "password")
+    @Column(name = "memberPw")
     private String memberPw;
-    @Column(name = "memberNickname")
+    @Column(name = "memberNickname", unique = true) //닉네임 중복 방지
     private String memberNickname;
     @Column(name = "memberAcademy")
     private String memberAcademy;
     @Column(name = "memberRole")
-    private String memberRole; //권한
+    private String memberRole; //멤버역할 (권한아님) << 기술스택(프론트, 백)
     @Column(name = "memberIntroduce")
     private String memberIntroduce;
     @Column(name = "memberLink")
     private String memberLink;
-    @Column(name = "memberIcon")
-    private String memberIcon;
+    @Column(name = "memberFilePath",nullable = true)
+    private String memberFilePath;
+    @Column(name = "memberFileName",nullable = true)
+    private String memberFileName;
+    @Column(name = "memberStack", nullable = true)
+    private String memberStack;
+
+
     @Column(name = "authority")
-    private String memberAuthority;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Builder.Default
+    public Set<MemberRoleEntity> memberAuthority = new HashSet<>();
+
+    public void addMemberRole(MemberRoleEntity memberRoleEntity) {
+        this.memberAuthority.add(memberRoleEntity);
+    }
 
     public MemberEntity(MemberRequestDTO requestDTO) {
         this.memberId = requestDTO.getMemberId();
@@ -42,7 +59,33 @@ public class MemberEntity extends TimeStamp {
         this.memberRole = requestDTO.getMemberRole();
         this.memberIntroduce = requestDTO.getMemberIntroduce();
         this.memberLink = requestDTO.getMemberLink();
-        this.memberIcon = requestDTO.getMemberIcon();
-        this.memberAuthority = "member";
+        this.memberFileName = requestDTO.getMemberFileName();
+        this.memberFilePath = requestDTO.getMemberFilePath();
+        this.memberStack = requestDTO.getMemberStack();
+        this.memberAuthority = new HashSet<>();
     }
+
+    public MemberEntity(MemberLoginDTO loginDTO) {
+        this.memberId = loginDTO.getUsername();
+        this.memberPw = loginDTO.getPassword();
+        this.memberNickname = loginDTO.getMemberNickName();
+        this.memberAcademy = loginDTO.getMemberAcademy();
+        this.memberRole = loginDTO.getMemberRole();
+        this.memberIntroduce = loginDTO.getMemberIntroduce();
+        this.memberLink = loginDTO.getMemberLink();
+        this.memberFilePath = loginDTO.getMemberFilePath();
+        this.memberFileName = loginDTO.getMemberFileName();
+        this.memberAuthority = new HashSet<>();
+    }
+
+    public void memberUpdate(MemberRequestDTO requestDTO) {
+        this.memberPw = requestDTO.getMemberPw();
+        this.memberNickname = requestDTO.getMemberNickname();
+        this.memberAcademy = requestDTO.getMemberAcademy();
+        this.memberRole = requestDTO.getMemberRole();
+        this.memberIntroduce = requestDTO.getMemberIntroduce();
+        this.memberLink = requestDTO.getMemberLink();
+        this.memberAuthority = new HashSet<>();
+    }
+
 }
