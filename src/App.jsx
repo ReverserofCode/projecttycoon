209e17cf882ec16801c3ad146c58018b879e71c7
minicastle <jk598200167@gmail.com>
2023-10-17 16:25:21 +0900

    [feat] 반응형 적용

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ const MainContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   width: 100%;
+  height: 200vh;
   min-height: 100%;
+  position: relative;
 `;
 /** 메인 콘텐츠가 담기는 사이즈 제한 콘테이너 태그 */
 const Wrap = styled.div`
@@ -23,8 +25,7 @@ function App() {
   return (
     <MainContainer>
       <Navbar userData={userData} handleSetUserData={handleSetUserData} />
-      <Wrap>
-      </Wrap>
+      <Wrap></Wrap>
     </MainContainer>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import { LoginCheck } from "../functional/LoginCheck";
 import { Logout } from "../functional/Logout";
@@ -11,9 +11,19 @@ const Container = styled.div`
   padding: 0 30px;
   width: 100%;
   border-bottom: 1px solid #071952;
+  position: fixed;
+  top: 0;
   @media screen and (max-width: 700px) {
     height: 64px;
   }
+  @media screen and (max-width: 715px) {
+    height: 45px;
+    padding: 5px 10px;
+  }
+  @media screen and (max-width: 360px) {
+    height: 40px;
+  }
+  z-index: 2;
 `;
 /** 네비게이션 바의 로고 태그 */
 const NavHeader = styled.img`
@@ -22,6 +32,15 @@ const NavHeader = styled.img`
   :hover {
     scale: 1.11;
   }
+  @media screen and (max-width: 800px) {
+    width: 40px;
+    height: 40px;
+  }
+  @media screen and (max-width: 360px) {
+    width: 30px;
+    height: 30px;
+  }
+  z-index: 3;
 `;
 /** 네비게이션 바의 페이지를 담고잇는 콘테이너 리스트 태그 */
 const Lists = styled.ul`
@@ -33,6 +52,18 @@ const Lists = styled.ul`
   color: ${(props) => {
     return props.color ? props.color : "#071952";
   }};
+  @media screen and (max-width: 800px) {
+    gap: 20px;
+  }
+  @media screen and (max-width: 715px) {
+    display: none;
+  }
+  z-index: 4;
+`;
+/** 네비게이션 바의 페이지 이동 태그 */
+const Link = styled.a`
+  text-decoration: none;
+  z-index: 5;
 `;
 /** 네비게이션 바의 페이지를 담당하는 리스트 아이템 태그 */
 const Items = styled.li`
@@ -43,10 +74,11 @@ const Items = styled.li`
   :hover {
     font-weight: 600;
   }
-`;
-/** 네비게이션 바의 페이지 이동 태그 */
-const Link = styled.a`
-  text-decoration: none;
+  @media screen and (max-width: 950px) {
+    font-size: 15px;
+  }
+
+  z-index: 6;
 `;
 /** 네비게이션 바의 width를 제한하는 태그 */
 const Wrap = styled.div`
@@ -55,11 +87,107 @@ const Wrap = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  z-index: 2;
 `;
 const UserIcon = styled.image`
   height: 50px;
+  z-index: 7;
+`;
+const MenuMinimize = styled.div`
+  display: none;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  @media screen and (max-width: 715px) {
+    display: flex;
+  }
+`;
+const MenuButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  font-size: 40px;
+  position: relative;
+  span {
+    transition: 300ms;
+    position: absolute;
+    width: 100%;
+    border: 1px solid black;
+    z-index: 100;
+  }
+  span.top {
+    top: 0px;
+  }
+  span.middle {
+  }
+  span.bottom {
+    bottom: 0;
+  }
+  &.open span.middle.first {
+    transform: rotate(45deg);
+  }
+  &.open span.middle.sec {
+    transform: rotate(-45deg);
+  }
+  &.open span.top {
+    opacity: 0;
+  }
+  &.open span.bottom {
+    opacity: 0;
+  }
+  z-index: 100;
+  @media screen and (max-width: 715px) {
+    width: 30px;
+    height: 24px;
+  }
+  @media screen and (max-width: 360px) {
+    width: 20px;
+    height: 16px;
+  }
+`;
+const MenuList = styled.div`
+  transform: 1000ms;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  box-sizing: border-box;
+  padding: 50px;
+  width: 200px;
+  height: 100vh;
+  background-color: #c0c0c0;
+  top: -13px;
+  right: -10;
+  p {
+    white-space: nowrap;
+  }
+  animation: ${(props) => {
+      return props.opened ? "OpenMenu" : "CloseMenu";
+    }}
+    300ms ease-in-out both;
+  @keyframes OpenMenu {
+    from {
+      transform: translate(-70px);
+    }
+    to {
+      transform: translate(150px);
+    }
+  }
+  @keyframes CloseMenu {
+    from {
+      transform: translate(150px);
+    }
+    to {
+      transform: translate(-70px);
+    }
+  }
+  z-index: 2;
 `;
 function Navbar({ userData, handleSetUserData }) {
+  const [minimizeMenu, setMinimizeMenu] = useState(true);
+  const MenuItem = useRef(null);
   useEffect(() => {
     LoginCheck().then((res) => {
       handleSetUserData(res);
@@ -89,6 +217,40 @@ function Navbar({ userData, handleSetUserData }) {
             <Items>새로운 글쓰기</Items>
           </Link>
         </Lists>
+        <MenuMinimize>
+          <MenuButton
+            ref={MenuItem}
+            onClick={() => {
+              MenuItem.current.classList.toggle("open");
+              setMinimizeMenu(!minimizeMenu);
+            }}
+          >
+            <span className="top"></span>
+            <span className="middle first"></span>
+            <span className="middle sec"></span>
+            <span className="bottom"></span>
+          </MenuButton>
+          <MenuList opened={minimizeMenu}>
+            <Link className="menu" href="http://projecttycoon.com">
+              <p>홈</p>
+            </Link>
+            <Link className="menu" href="http://projecttycoon.com/projects">
+              <p>프로젝트 팀</p>
+            </Link>
+            <Link className="menu" href="http://projecttycoon.com/members">
+              <p>멤버리스트</p>
+            </Link>
+            <Link className="menu" href="http://projecttycoon.com/myPage">
+              <p>내 페이지</p>
+            </Link>
+            <Link
+              className="menu"
+              href="http://www.projecttycoon.com/callPageNewProject"
+            >
+              <p>새로운 글쓰기</p>
+            </Link>
+          </MenuList>
+        </MenuMinimize>
         {userData === "" ? (
           <Lists gap={"47px"} color="#35A29F">
             <Link className="user" href="http://projecttycoon.com/api/login">
