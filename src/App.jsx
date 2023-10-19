65eec0b3f4116571092c4d92cfc91202fb98f093
minicastle <jk598200167@gmail.com>
2023-10-19 17:51:19 +0900

    [fix] 네비게이션 바 수정

--- src/App.jsx
+++ src/App.jsx
@@ -25,46 +25,7 @@ const Wrap = styled.div`
 `;
 
 function App() {
-  const [userData, setUserData] = useState({
-    memberId: "sift",
-    memberPw: "$2a$10$unl/kpsWN9lFX5ax34J1VueoVQCrp.p2PT/.gVWWf8.kkNG7AfJ/G",
-    memberRole: "front",
-    memberIntroduce: "asdfasdf",
-    memberLink: '[{"option":"Git","value":"asdfasdf@"}]',
-    memberAcademy: "노원",
-    memberNickname: "정성민",
-    memberFilePath: "/static/icons/",
-    memberFileName: "9_alpaca.png",
-    memberStack: '["HTML"]',
-    myProjectlist: [],
-    myCommentlist: [],
-    myScraplist: [
-      {
-        createdAt: "2023-10-18T07:18:08.827+00:00",
-        modifiedAt: "2023-10-18T07:23:21.814+00:00",
-        projectId: 3,
-        projectTitle: "헷",
-        projectContent: "<p>헷</p>",
-        projectWantedRole: '[{"role": "back", "complete": 0, "personnel": 1}]',
-        projectDue: "2023-11-11T00:00:00.000+00:00",
-        projectAcademy: "노원",
-        projectStatus: true,
-        projectWriterId: null,
-        projectWriterNick: null,
-        projectFilePath:
-          "/webapp/b0b176b3-e77d-4e1d-aee0-ef1644d4b6bb_Black.png",
-        projectFileName: "projectImage",
-        projectScrapNum: 2,
-        parsedProjectWantedRole: [
-          {
-            role: "back",
-            complete: 0,
-            personnel: 1,
-          },
-        ],
-      },
-    ],
-  });
+  const [userData, setUserData] = useState("");
   const handleSetUserData = useCallback((value) => {
     setUserData(value);
   }, []);
