85049fc24b56e6012ca2294d9f1bec42dfda1021
minicastle <jk598200167@gmail.com>
2023-10-19 20:41:15 +0900

    [fix] 네비게이션 바

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,37 @@ import React from "react";
 import styled from "@emotion/styled";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
-
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   width: 100%;
+  height: 200vh;
   min-height: 100%;
+  position: relative;
+  margin-top: 64px;
+  @media screen and (max-width: 715px) {
+    margin-top: 45px;
+  }
+  @media screen and (max-width: 360px) {
+    margin-top: 40px;
+  }
 `;
+/** 메인 콘텐츠가 담기는 사이즈 제한 콘테이너 태그 */
 const Wrap = styled.div`
   max-width: 1440px;
+  width: 100%;
 `;
 
 function App() {
+  const [userData, setUserData] = useState("");
+  const handleSetUserData = useCallback((value) => {
+    setUserData(value);
+  }, []);
   return (
     <MainContainer>
-      <Navbar />
+      <Navbar userData={userData} handleSetUserData={handleSetUserData} />
       <Wrap>
         <Register />
       </Wrap>
