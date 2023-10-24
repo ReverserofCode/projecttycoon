e40cba99a67af984655363d1cb4eaa24d1223ad7
minicastle <jk598200167@gmail.com>
2023-10-24 12:12:13 +0900

    [fix]width

--- src/page/Page1.jsx
+++ src/page/Page1.jsx
@@ -18,6 +18,7 @@ const Container = styled.div`
   width: 100%;
   padding: 0 140px;
   box-sizing: border-box;
+  max-width: 1100px;
 `;
 const TextZone = styled.div`
   display: flex;
@@ -34,8 +35,8 @@ const Img = styled.img`
   width: 100%;
 `;
 const Button = styled.div`
-max-width: 300px;
-width: 100%;
+  max-width: 300px;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -43,22 +44,31 @@ width: 100%;
     scale: 0.98;
   }
 `;
+const Width = styled.div`
+  width: 100%;
+  background-color: #0b666a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 function Page1({ target }) {
   return (
     <Wrap ref={target}>
-      <Container>
-        <TextZone>
-          <PixelText40>전국에 있는 코리아IT학원</PixelText40>
-          <PixelText40>사람들과 함께하는</PixelText40>
-          <PixelText40>프로젝트를 개설해보세요.</PixelText40>
-          <Button>
-            <Img src={ButtonIcon} />
-          </Button>
-        </TextZone>
-        <ImgBox>
-          <Img src={Image} />
-        </ImgBox>
-      </Container>
+      <Width>
+        <Container>
+          <TextZone>
+            <PixelText40>전국에 있는 코리아IT학원</PixelText40>
+            <PixelText40>사람들과 함께하는</PixelText40>
+            <PixelText40>프로젝트를 개설해보세요.</PixelText40>
+            <Button>
+              <Img src={ButtonIcon} />
+            </Button>
+          </TextZone>
+          <ImgBox>
+            <Img src={Image} />
+          </ImgBox>
+        </Container>
+      </Width>
     </Wrap>
   );
 }
