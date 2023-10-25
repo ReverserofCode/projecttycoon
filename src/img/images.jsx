5953ea93450e55d639836dba2bdbf32ec5a1a017
nayoung <kny_7_@naver.com>
2023-10-25 15:18:31 +0900

    userWrite 기본이미지

--- src/img/images.jsx
+++ src/img/images.jsx
@@ -1,5 +1,5 @@
 export const default1 =
-  "/webapp/56eecdc3-2700-4051-9b69-d1abccf29fb0_default.png";
-export const default2 = "/webapp/6918b100-6b5a-4c08-9ab8-c8a9d9ef704a_two.png";
+  "/static/images/project_default1.png";
+export const default2 = "/static/images/project_default2.png";
 export const default3 =
-  "/webapp/a9d03050-441a-485e-997b-a5aa5d8d7d0b_three.png";
+  "/static/images/project_default3.png";
