49878d419170f9abe2f0af39fb76ec347b859697
nayoung <kny_7_@naver.com>
2023-11-01 11:20:33 +0900

    Page3 반응형

--- src/components/Pixel.jsx
+++ src/components/Pixel.jsx
@@ -5,4 +5,19 @@ export const PixelText40 = styled.span`
   white-space: nowrap;
   padding: 10px 0 0 0;
   color: ${(props) => props.color};
+  @media (max-width: 760px) {
+    font-size: 20px;
+  }
+  @media (max-width: 650px) {
+    font-size: 17px;
+  }
+  @media (max-width: 550px) {
+    font-size: 13px;
+  }
+  @media (max-width: 460px) {
+    font-size: 12px;
+  }
+  @media (max-width: 380px) {
+    font-size: 11px;
+  }
 `;
