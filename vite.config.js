fda1a0e5f7db5d842c2ff2fec27c4b316ce7968a
minicastle <jk598200167@gmail.com>
2023-11-01 13:50:22 +0900

    [fix] swipe

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir:'PageHome'
+    outDir:'PageHome',
   },
   server:{
     port:3000,
