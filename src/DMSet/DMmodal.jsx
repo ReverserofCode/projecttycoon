ccf8f23681cf502f009ce8bca7752227593b0215
minicastle <jk598200167@gmail.com>
2023-11-01 20:24:09 +0900

    [feat&fix] scrap page,logout redirect

--- src/DMSet/DMmodal.jsx
+++ src/DMSet/DMmodal.jsx
@@ -4,6 +4,7 @@ import { BsChatText } from "react-icons/bs";
 import { AiOutlineStar } from "react-icons/ai";
 import ChatRoom from "./ChatRoom";
 import ChatList from "./ChatList";
+import ScrapList from "./ScrapList";
 /** DM 모달의 콘테이너 태그 */
 const Container = styled.div`
   transition: 200ms;
@@ -97,7 +98,13 @@ function DMmodal({ status, DMList, Mod, handleSetMod, myId, handleGetList }) {
         <span>쪽지</span>
         <IconButtons>
           <IconButton>
-            <AiOutlineStar fontSize={"25px"}></AiOutlineStar>
+            <AiOutlineStar
+              fontSize={"25px"}
+              onClick={() => {
+                handleSetMod("scrap");
+                // handleGetList();
+              }}
+            ></AiOutlineStar>
           </IconButton>
           <IconButton
             onClick={() => {
@@ -128,6 +135,8 @@ function DMmodal({ status, DMList, Mod, handleSetMod, myId, handleGetList }) {
           handleSetData={handleSetData}
           key={`chat room ${targetId}`}
         />
+      ) : Mod === "scrap" ? (
+        <ScrapList />
       ) : (
         "none"
       )}
